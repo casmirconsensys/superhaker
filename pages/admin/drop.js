e360d171226cd27aceebd3ce15b84838a7a3455c
@askcasmir <casmirconsults@gmail.com>
2023-08-13 01:42:17 -0400

    WIP: Deployed Marketplace Contract and NFT Drop on Zora & BASE, TODO: TheGraph data feed, SAFE AUTH signin

--- pages/admin/drop.js
+++ pages/admin/drop.js
@@ -1,14 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-import { useMoralis, useMoralisCloudFunction } from 'react-moralis'
+// import { useMoralis, useMoralisCloudFunction } from 'react-moralis'
 import router from 'next/router'
 import Loader from '../../components/basic/loader/Loader'
 
 
 
 const Drop = () => {
-    const { Moralis, enableWeb3, web3 } = useMoralis()
-    const { data: feedData, error, isLoading, fetch} = useMoralisCloudFunction('getNonSentNFTs')
+    // const { Moralis, enableWeb3, web3 } = useMoralis()
+    // const { data: feedData, error, isLoading, fetch} = useMoralisCloudFunction('getNonSentNFTs')
 
     const walletAddress = useSelector(state => state.user.walletAddress)
 
@@ -22,8 +22,43 @@ const Drop = () => {
             contractAddress: '0xB262E527ac2C2c788316421aD9129B5B3ccFfd0C',
             tokenId: id
         }
-        await Moralis.transfer(options)
-        .then(async (data) => {
+        const express = require("express");
+        const app = express();
+        // This is a public sample test API key.
+        // Don’t submit any personally identifiable information in requests made with this key.
+        // Sign in to see your own test API key embedded in code samples.
+        const Stripe = require("stripe");
+        const stripe = Stripe('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+        const OnrampSessionResource = Stripe.StripeResource.extend({
+          create: Stripe.StripeResource.method({
+            method: 'POST',
+            path: 'crypto/onramp_sessions',
+          }),
+        });
+        
+        
+        app.use(express.static("public"));
+        app.use(express.json());
+        
+        app.post("/create-onramp-session", async (req, res) => {
+          const { transaction_details } = req.body;
+        
+          // Create an OnrampSession with the order amount and currency
+          const onrampSession = await new OnrampSessionResource(stripe).create({
+            transaction_details: {
+              destination_currency: transaction_details["destination_currency"],
+              destination_exchange_amount: transaction_details["destination_exchange_amount"],
+              destination_network: transaction_details["destination_network"],
+            },
+            customer_ip_address: req.socket.remoteAddress,
+          });
+        
+          res.send({
+            clientSecret: onrampSession.client_secret,
+          });
+        });
+        
+        app.listen(4242, () => console.log("Node server listening on port 4242!"));        .then(async (data) => {
             const ItemClass = await Moralis.Object.extend('Item')
             const query = new Moralis.Query(ItemClass).equalTo('tokenId', id)
             const results = await query.find()
